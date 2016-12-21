bed8848b3e253b1eebd6411c0c605f022e3da4d6
Michael Smith <michael@msmith.nz>
2016-12-21 19:39:52 +1300

    add pick a contact button

--- app/App.js
+++ app/App.js
@@ -8,33 +8,50 @@ import React, { Component } from 'react'
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  Image
 } from 'react-native'
 
 import ReadTexts from './ReadTexts'
 
+import Logo from './assets/AskRuru.png'
+
 class App extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = {
-      sms: [],
-      smsCount: 0
-    }
-    ReadTexts.getTextMessages(() => {
-      console.log('error')
-    },
-    (sms) => {
-      this.setState({sms: sms, smsCount: sms.length})
-      console.log(this.state.sms)
-    })
+  // constructor (props) {
+    // super(props)
+    // this.state = {
+    //   sms: [],
+    //   smsCount: 0
+    // }
+    // ReadTexts.getTextMessages(() => {
+    //   console.log('error')
+    // },
+    // (sms) => {
+    //   this.setState({sms: sms, smsCount: sms.length})
+    //   console.log(this.state.sms)
+    // })
+  // }
+
+  pickContact () {
+    console.log('Pick a contact has been pressed')
   }
+
   render () {
     return (
       <View style={styles.container}>
-        <Text>
+        {/* <Text>
           Hello! There is {this.state.smsCount} texts
-        </Text>
+        </Text> */}
+        <Image
+          source={Logo}
+          style={{width: 100, height: 100}}
+        />
+        <Button
+          onPress={this.pickContact}
+          title='Select Contact'
+        />
       </View>
     )
   }
@@ -45,7 +62,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#fff'
   }
 })
 
--- index.ios.js
+++ index.ios.js
@@ -4,41 +4,20 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-import App from './app/App'
+  Text
+} from 'react-native'
 
 class AskRuru extends Component {
-  render() {
+  render () {
     return (
-      <App />
-    );
+      <Text>
+        This app is not currently used with iOS
+      </ Text>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('AskRuru', () => AskRuru);
+AppRegistry.registerComponent('AskRuru', () => AskRuru)
